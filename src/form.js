ae54739e7bb93c4ae56274a8c67b2c9fd4409aa2
Ethan Paul <24588726+enpaul@users.noreply.github.com>
2020-09-15 20:10:19 -0400

    Add form validation and post-action state reps
    Add icons

--- src/form.js
+++ src/form.js
@@ -1,15 +1,46 @@
+// This is a simple script compile form submissions to gsheets
+// in the absence of a real backend. Using google sheets for
+// this is definitely one of the jankier things I've put on the internet
+// but it took literally thirty minutes to make work, so it'll
+// work for now.
+//
+// Backend GScript and overall process adapted from this absolutely
+// incredible tutorial:
+// https://medium.com/@dmccoy/how-to-submit-an-html-form-to-google-sheets-without-google-forms-b833952cc175
+
 var FORM_URL = "https://script.google.com/macros/s/AKfycbwiQD9aQGaDc2hrx37OeRRk2gf3R91xPyRuFDWio2tirMLYnw/exec"
 
 $("#submit").on("click", function(e) {
   e.preventDefault()
+
+  subname = $("#subname").val()
+  subemail = $("#subemail").val()
+
+  if (!subemail) {
+    $("#subemail").addClass("invalid")
+    return
+  }
+
   $.ajax({
     url: FORM_URL,
     method: "GET",
     dataType: "json",
     data: {
-      name: $("#subname").val(),
-      email: $("#subemail").val(),
+      name: subname,
+      email: subemail,
       tstamp: new Date().toISOString()
+    },
+    success: function() {
+      $("#subemail").removeClass("invalid")
+      $("#submit").text("Thank you!")
+      $("#submit").addClass("success")
+      $("#subemail").val("")
+      $("#subname").val("")
+    },
+    error: function() {
+      $("#subemail").removeClass("invalid")
+      $("#submit").text("Something went wrong!")
+      $("#submit").addClass("failure")
     }
   })
 })
